e48907646cdc010862dafa9cfc67fe49ff6859a3
Rodrigo Matola <rodrigomatola@MacBook-Pro-de-Rodrigo.local>
2023-12-20 14:04:48 -0300

    refactor: custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,17 @@
-Cypress.Commands.add('setUserCookie', user => { 
-    cy.setCookie('session-username', user)
-})
+import { loginPage, yourInfoPage } from "./pageElements/pageElements";
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.get(loginPage.usernameField).type(username);
+    cy.get(loginPage.passwordField).type(password);
+    cy.get(loginPage.loginBtn).click();
+});
+
+Cypress.Commands.add('setUserCookie', user => {
+    cy.setCookie('session-username', user);
+});
+
+Cypress.Commands.add('fillUserInfo', (firstName, lastName, zipCode) => {
+    cy.get(yourInfoPage.firstNameField).type(firstName)
+    cy.get(yourInfoPage.lastNameField).type(lastName)
+    cy.get(yourInfoPage.zipCodeField).type(zipCode)
+});
--- cypress/support/pageElements/pageElements.js
+++ cypress/support/pageElements/pageElements.js
@@ -1,4 +1,25 @@
+export const loginPage = {
+    usernameField: '[data-test="username"]',
+    passwordField: '[data-test="password"]',
+    loginBtn: '[data-test="login-button"]'
+}
+
 export const inventoryPage = {
     addBackpackToCart: '[data-test="add-to-cart-sauce-labs-backpack"]',
     shoppingCartBtn: '#shopping_cart_container'
+}
+
+export const yourCartPage = {
+    checkoutBtn: '[data-test="checkout"]'
+}
+
+export const yourInfoPage = {
+    firstNameField: '[data-test="firstName"]',
+    lastNameField: '[data-test="lastName"]',
+    zipCodeField: '[data-test="postalCode"]',
+    continueBtn: '[data-test="continue"]'
+}
+
+export const overviewPage = {
+    finishBtn: '[data-test="finish"]'
 }
